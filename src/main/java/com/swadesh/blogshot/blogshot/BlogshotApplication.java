c5f37cdc678857861f008033143b8af53c3643a3
swadesh-vaibhav <swadesh.vaibhav@gmail.com>
2024-02-11 16:00:29 +0530

    Updated project settings

--- src/main/java/com/swadesh/blogshot/blogshot/BlogshotApplication.java
+++ src/main/java/com/swadesh/blogshot/blogshot/BlogshotApplication.java
@@ -1,4 +1,4 @@
-package com.example.blogshot.blogshot;
+package com.swadesh.blogshot.blogshot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
